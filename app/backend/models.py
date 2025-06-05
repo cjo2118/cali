70998d0a0325af9594dd19abb8376b1907a45631
Chloe Oppenheim <chloe@MacBook-Air-3.home>
2025-06-04 21:33:45 -0400

    adding db models

--- app/backend/models.py
+++ app/backend/models.py
@@ -1,11 +1,24 @@
+from enum import Enum
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
 from database import Base
 
-class Plan(Base):
-    __tablename__ = "plans"
+class Hang(Base):
+    __tablename__ = "hang"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    group_id = Column(Integer, ForeignKey("group.id"))
+    group = relationship("Group", back_populates="hangs")
+
+
+class Group(Base):
+    __tablename__ = "group"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
@@ -13,11 +26,60 @@ class Plan(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-class Group(Base):
-    __tablename__ = "groups"
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class Friend(Base):
+    __tablename__ = "friend"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    friend_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class FriendGroup(Base):
+    __tablename__ = "friend_group"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow) 
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+class FriendGroupMember(Base):
+    __tablename__ = "friend_group_member"
+
+    id = Column(Integer, primary_key=True, index=True)
+    friend_group_id = Column(Integer, ForeignKey("friend_groups.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class FavoriteType(Enum):
+    INDIVIDUAL = "individual"
+    GROUP = "group"
+
+class FavoriteStatus(Enum):
+    TRIED = "tried"
+    NOT_TRIED = "not_tried"
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+
+    id = Column(Integer, primary_key=True, index=True)
+    type = Column(Enum(FavoriteType))
+    status = Column(Enum(FavoriteStatus))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    hang_id = Column(Integer, ForeignKey("hang.id"))
+    group_id = Column(Integer, ForeignKey("group.id"))
+    created_by_user_id = Column(Integer, ForeignKey("user.id"))
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
